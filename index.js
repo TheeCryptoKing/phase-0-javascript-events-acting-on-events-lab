4aa0e7c080ab903e53980f28792e46a5a2b5d571
kwamebrowne <theeecryptoking@gmail.com>
2023-04-12 20:14:19 -0500

    I MADE A BUTTTON

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Your code here
+//they do this for us
+const dodger = document.getElementById("dodger");
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+}
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+
+  //My code
+function moveDodgerRight() {
+    const rightNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(rightNumbers, 10);// didnt work
+  
+    if (left > 0) {
+      dodger.style.left = `${left + 1}px`;
+    }
+}
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+  });
+  //LETS GO I MADE A RIGHT BUTTON
+  // Could we add right into it
